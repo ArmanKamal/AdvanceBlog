732d3aa103b1a4c4880b67fc00d79da2ed9edb13
Arman Kamal <armankamal2421@gmail.com>
2020-11-10 15:05:48 -0800

    Comment Model Added

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Post,CategoryModel,UserProfile
+from .models import Post,CategoryModel,UserProfile,Comment
 
 admin.site.register(Post)
 admin.site.register(CategoryModel)
 admin.site.register(UserProfile)
+admin.site.register(Comment)
 # Register your models here.
